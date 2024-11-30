57aa96b0a5ec638f955ff137afbbf1952cee7a74
Naoki.Yasuno <Naoki.Yasuno@sony.com>
2024-12-01 00:01:27 +0900

    Fix flake error

--- annotation_tool.py
+++ annotation_tool.py
@@ -1,20 +1,20 @@
-from tkinter import *
-from tkinter import font
-from PIL import Image, ImageTk, ImageOps
+import tkinter
+from PIL import Image, ImageTk
 import os
 import json
-#----------------------------------------------------------------------
+
+
 # 設定項目
 images_dir = ".\\images" # 画像フォルダのパス
-json_path  = ".\\result.json" # 出力(json)ファイルのパス
+json_path = ".\\result.json" # 出力(json)ファイルのパス
 classes = ["犬","猫","鳥","猿","羊","狼","狸","不明"] # 分類するクラス
-image_width = 257 * 2 # 表示画像の幅
-image_height = 61 * 2 # 表示画像の高さ
-#----------------------------------------------------------------------
+image_width = 257 * 2  # 表示画像の幅
+image_height = 61 * 2  # 表示画像の高さ
+
+
 class MainWindow():
-    #----------------
     def __init__(self, main, args):
-        self.current_image_num = 0 # 開始画像位置の設定
+        self.current_image_num = 0
         self.main = main
         self.images_dir = args["images_dir"]
         self.json_path = args["json_path"]
@@ -27,49 +27,56 @@ class MainWindow():
         self.kyboard_str = "123456789qwertyuiopasdfghjklzxcvbnm"
         self.init_window()
         self.init_shortcuts()
-    #----------------
+
     def init_window(self):
-        # フォントの設定
-        font_label_class = font.Font(size=20, weight='bold')
-        font_label_index = font.Font(size=15)
         # タイトルの設定
         self.main.title(u"アノテーションツール")
 
         # 画像を表示するキャンバスを作る
-        self.canvas = Canvas(self.main,width=self.image_width,height=self.image_height)
+        self.canvas = tkinter.Canvas(self.main, width=self.image_width,
+                                     height=self.image_height)
 
         # 画像名を表示する
-        self.label_image_name = Label(self.main, text=self.images_list[self.current_image_num],
-                                      font=('Times', 12))
-        self.label_image_name.grid(row=0, columnspan=7)
+        self.image_name = tkinter.Label(self.main,
+                                              text=self.images_list[self.current_image_num],
+                                              font=('Times', 12))
+        self.image_name.grid(row=0, columnspan=7)
 
         self.canvas.grid(row=1, column=0, columnspan=7, rowspan=1)
 
         # 書かれた指してを入力するテキストフィールド
-        self.move_text = Entry(width=40, font=('Times', 20))
+        self.move_text = tkinter.Entry(width=40, font=('Times', 20))
         self.move_text.grid(row=2, column=3)
 
         # 最初の画像をセット
-        self.image_on_canvas = self.canvas.create_image(0, 0, anchor=NW, image=self.img)
+        self.image_on_canvas = self.canvas.create_image(0, 0,
+                                                        anchor=tkinter.NW,
+                                                        image=self.img)
         self.set_image()
+
     def init_shortcuts(self):
         self.main.focus_set()
         self.main.bind('<Return>', self.onNextButton)
         self.main.bind('<space>', self.onBackButton)
+
     def set_message(self):
-        self.label_image_name["text"] = self.images_list[self.current_image_num]
-    def set_image(self,e=None):
-        img = Image.open(os.path.join(self.images_dir,self.images_list[self.current_image_num]))
-        img = img.resize((self.image_width,self.image_height), Image.LANCZOS)
+        self.image_name["text"] = self.images_list[self.current_image_num]
+
+    def set_image(self, e=None):
+        img = Image.open(os.path.join(self.images_dir,
+                                      self.images_list[self.current_image_num]))
+        img = img.resize((self.image_width, self.image_height), Image.LANCZOS)
         self.img = ImageTk.PhotoImage(img)
         self.canvas.itemconfig(self.image_on_canvas, image=self.img)
+
     def get_class_name(self, img_path):
         data = self.load_json()
         if img_path in data:
             return self.classes[data[img_path]]
         else:
             return "No Label"
-    def onNextButton(self,e=None):
+
+    def onNextButton(self, e=None):
         # 一つ進む
         self.current_image_num += 1
         # 最初の画像に戻る
@@ -78,7 +85,8 @@ class MainWindow():
         # 表示画像を更新
         self.set_image()
         self.set_message()
-    def onBackButton(self,e=None):
+
+    def onBackButton(self, e=None):
         # 一つ戻る
         self.current_image_num -= 1
         # 最後の画像へ
@@ -87,29 +95,42 @@ class MainWindow():
         # 表示画像を更新
         self.set_image()
         self.set_message()
+
     def labeling(self, class_num):
         def x(e=None):
-            img_path =self.images_list[self.current_image_num]
+            img_path = self.images_list[self.current_image_num]
             self.update_json(img_path, class_num)
             self.set_message()
         return x
+
     def load_json(self):
         data = {}
         try:
-            data = json.load(open(self.json_path,'r'))
+            data = json.load(open(self.json_path, 'r'))
         except json.JSONDecodeError as e:
             pass
         except FileNotFoundError as e:
             with open(self.json_path, 'w'):
                 pass
         return data
-    def update_json(self,img_path, class_num):
+
+    def update_json(self, img_path, class_num):
         data = self.load_json()
         data[img_path] = class_num
-        json.dump(data, open(self.json_path,'w'),indent=4)
-#----------------------------------------------------------------------
+        json.dump(data, open(self.json_path, 'w'), indent=4)
 
-root = Tk()
-args = {"images_dir":images_dir, "json_path":json_path, "classes":classes, "width":image_width, "height":image_height}
-MainWindow(root,args)
-root.mainloop()
+
+def main():
+    root = tkinter.Tk()
+    args = {"images_dir": images_dir,
+            "json_path": json_path,
+            "classes": classes,
+            "width": image_width,
+            "height": image_height,
+            }
+    MainWindow(root, args)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
