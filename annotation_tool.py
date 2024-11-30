cf726e8827065ce26e2f6b5312e6451aa0f31564
Naoki.Yasuno <Naoki.Yasuno@sony.com>
2024-11-30 23:45:22 +0900

    Change UI layout for chess OCR

--- annotation_tool.py
+++ annotation_tool.py
@@ -8,8 +8,8 @@ import json
 images_dir = ".\\images" # 画像フォルダのパス
 json_path  = ".\\result.json" # 出力(json)ファイルのパス
 classes = ["犬","猫","鳥","猿","羊","狼","狸","不明"] # 分類するクラス
-image_width = 800 # 表示画像の幅
-image_height = 450 # 表示画像の高さ
+image_width = 257 * 2 # 表示画像の幅
+image_height = 61 * 2 # 表示画像の高さ
 #----------------------------------------------------------------------
 class MainWindow():
     #----------------
@@ -34,32 +34,21 @@ class MainWindow():
         font_label_index = font.Font(size=15)
         # タイトルの設定
         self.main.title(u"アノテーションツール")
+
         # 画像を表示するキャンバスを作る
         self.canvas = Canvas(self.main,width=self.image_width,height=self.image_height)
-        self.canvas.grid(row=0, column=0, columnspan=7, rowspan=1)
-        # 次の画像を表示するボタン
-        self.button_next = Button(
-            self.main, text="Next (→)", command=self.onNextButton, height=3)
-        self.button_next.grid(row=2, column=5, pady=10, sticky='nsew')
-        self.button_back = Button(
-            self.main, text="Back (←)", command=self.onBackButton, height=3)
-        self.button_back.grid(row=2, column=1, pady=10, sticky='nsew')
-        # クラスを決定するボタン
-        self.button_class = []
-        for i, c in enumerate(self.classes):
-            key = self.kyboard_str[i] if i < 35 else ""
-            self.button_class.append(Button(self.main, text="{} ({})".format(c,key), command=self.labeling(class_num=i), width=10))
-            self.button_class[i].grid(row=(i//7)+3, column=i%7, padx=5, pady=10, sticky='nsew')
-        # ラベルの内容の初期化
-        self.message_image_index = StringVar()
-        self.message_image_class = StringVar()
-        self.set_message()
-        # クラスを表示するラベル
-        self.label_image_class = Label(self.main, textvariable=self.message_image_class, width=50, font=font_label_class, background='#CCDDDD')
-        self.label_image_class.grid(row=1, columnspan=7)
-        # 現在の画像番号を表示するラベル
-        self.label_image_index = Label(self.main, textvariable=self.message_image_index, font=font_label_index)
-        self.label_image_index.grid(row=2, column=3, pady=10, sticky='nsew')
+
+        # 画像名を表示する
+        self.label_image_name = Label(self.main, text=self.images_list[self.current_image_num],
+                                      font=('Times', 12))
+        self.label_image_name.grid(row=0, columnspan=7)
+
+        self.canvas.grid(row=1, column=0, columnspan=7, rowspan=1)
+
+        # 書かれた指してを入力するテキストフィールド
+        self.move_text = Entry(width=40, font=('Times', 20))
+        self.move_text.grid(row=2, column=3)
+
         # 最初の画像をセット
         self.image_on_canvas = self.canvas.create_image(0, 0, anchor=NW, image=self.img)
         self.set_image()
@@ -67,9 +56,6 @@ class MainWindow():
         self.main.focus_set()
         self.main.bind('<Key-Right>', self.onNextButton)
         self.main.bind('<Key-Left>', self.onBackButton)
-        for i in list(range(len(self.button_class))):
-            if i < 35:
-                self.main.bind("<Key-{}>".format(self.kyboard_str[i]), self.labeling(i))
     def set_message(self):
         self.message_image_index.set("{}/{}".format(str(self.current_image_num+1),str(self.images_num)))
         self.message_image_class.set("{}".format(self.get_class_name(self.images_list[self.current_image_num])))
